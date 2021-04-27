c124699ed4c107e1285aac5b4c94a5bf9572d8ec
paedayz <pae16phasit@gmail.com>
2021-04-27 12:52:45 +0700

    add book function

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/paedayz/GO_FIBER_BookList.git/driver"
 
 	"github.com/subosito/gotenv"
 )
+
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
+}
 
 var db *sql.DB
 
@@ -28,6 +36,21 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World")
+	})
+
+	app.Post("/book", func(c *fiber.Ctx) error {
+		var bookID int
+		b := new(Book)
+		if err := c.BodyParser(b); err != nil {
+			return err
+		}
+
+		fmt.Println(b)
+		err := db.QueryRow("insert into books (title, author, year) values($1, $2, $3) RETURNING id;",
+			b.Title, b.Author, b.Year).Scan(&bookID)
+
+		logFatal(err)
+		return c.SendString(strconv.Itoa(bookID))
 	})
 
 	app.Listen(":3000")
